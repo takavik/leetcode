a7fed9b86dc3ae87f883d1924bbc582d35bb704b
takavik <takavik@outlook.com>
2023-09-27 10:16:31 +0800

    Use refute for false assertion

--- p403/solution_test.rb
+++ p403/solution_test.rb
@@ -6,7 +6,7 @@ require_relative "solution"
 class SolutionTest < Minitest::Test
   def test_simple
     assert(can_cross([0,1,3,5,6,8,12,17]))
-    assert(!can_cross([0,1,2,3,4,8,9,11]))
+    refute(can_cross([0,1,2,3,4,8,9,11]))
     assert(true)
   end
 end
