f57c398cd7e12e7c2a63404d3cecb60bb6241bd1
takavik <takavik@outlook.com>
2023-10-26 16:13:00 +0800

    Solve problem 823, binary trees with factors

---
+++ p823/solution.rb
@@ -0,0 +1,26 @@
+# @param {Integer[]} arr
+# @return {Integer}
+def num_factored_binary_trees(arr)
+  count_roots = {}
+  arr.sort!
+  count_roots[arr[0]] = 1
+
+  1.upto(arr.size - 1) do |j|
+    count = 1
+    n = arr[j]
+    0.upto(j-1) do |i|
+      m = arr[i]
+      break if m > Math.sqrt(n)
+      q, r = n.divmod(m)
+      if r.zero? && arr.include?(q)
+        c = count_roots[q] * count_roots[m]
+        count += c
+        count += c unless q == m
+        count %= 1_000_000_007
+      end
+    end
+    count_roots[n] = count
+  end
+
+  count_roots.values.sum % 1_000_000_007
+end
