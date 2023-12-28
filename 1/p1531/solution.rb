1775b4ce5f00d67df93b270f713028f42bfe95cb
takavik <takavik@outlook.com>
2023-12-28 23:45:57 +0800

    Update problem 1531 solution using hash as cache

--- 1/p1531/solution.rb
+++ 1/p1531/solution.rb
@@ -2,33 +2,26 @@
 # @param {Integer} k
 # @return {Integer}
 def get_length_of_optimal_compression(s, k)
-  s = s.each_char.map { |c| c.ord - "a".ord }
+  s = s.each_char.map { |ch| ch.ord - "a".ord }
 
-  table = Array.new(s.size) do 
-    Array.new(k+1) do 
-      Array.new(27) { [] }
-    end
-  end
-
-  optimal_length = lambda do |start, remain, last, last_count|
+  optimal_length = lambda do |start, remain, last, last_count, table={}|
     return Float::INFINITY if remain.negative?
     return 0 if start + remain >= s.size
 
-    memo = table[start][remain][last][last_count]
+    memo = table[[start, remain, last, last_count]]
     return memo if memo
 
     result = if s[start] == last
-      len = optimal_length[start + 1, remain, last, last_count + 1]
+      len = optimal_length[start + 1, remain, last, last_count + 1, table]
       len += 1 if last_count == 1 || last_count == 9 || last_count == 99
       len
     else
-      reserv = 1 + optimal_length[start + 1, remain, s[start], 1]
-      remove = optimal_length[start + 1, remain - 1, last, last_count]
+      reserv = 1 + optimal_length[start + 1, remain, s[start], 1, table]
+      remove = optimal_length[start + 1, remain - 1, last, last_count, table]
       reserv < remove ? reserv : remove
     end
 
-    table[start][remain][last][last_count] = result 
-
+    table[[start, remain, last, last_count]] = result 
   end
   
   optimal_length[0, k, 26, 0]
