dfbd92c961e65ae5f43f1e4641345da42e9e5b39
takavik <takavik@outlook.com>
2024-01-19 17:23:54 +0800

    Fix test case

--- 2/p2385/solution_test.rb
+++ 2/p2385/solution_test.rb
@@ -8,6 +8,6 @@ class SolutionTest < Minitest::Test
   def test_simple
     assert_equal(4, amount_of_time(TreeNode.from_array([1,5,3,nil,4,10,6,9,2]), 3))
     assert_equal(0, amount_of_time(TreeNode.new(1), 1))
-    assert_equal(5, amount_of_time(TreeNode.from_array([16,nil,20,7,12,nil,15,nil,19,nil,1,2]), 1))
+    assert_equal(6, amount_of_time(TreeNode.from_array([16,nil,20,7,12,nil,15,nil,19,nil,1,2]), 1))
   end
 end
