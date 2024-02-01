dbc1bc799f12cb79ad24a98297daf0ce3b4f4d3b
takavik <takavik@outlook.com>
2024-02-01 12:33:51 +0800

    Solve problem 2966

---
+++ 2/p2966/solution.rb
@@ -0,0 +1,19 @@
+# @param {Integer[]} nums
+# @param {Integer} k
+# @return {Integer[][]}
+def divide_array(nums, k)
+  n = nums.size 
+  return [] if n % 3 != 0
+
+  nums.sort!
+  result = []
+  
+  (0...n).step(3) do |i|
+    x, y, z = nums[i, 3]
+    return [] if z - x > k
+
+    result << [x, y, z]
+  end
+
+  result
+end
