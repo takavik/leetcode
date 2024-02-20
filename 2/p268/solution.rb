39ee1e14f70513ee8fad127dd386460948bcca6d
takavik <takavik@outlook.com>
2024-02-20 11:15:38 +0800

    Update solution 268

--- 2/p268/solution.rb
+++ 2/p268/solution.rb
@@ -1,6 +1,10 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def missing_number(nums)
+  # Method 0: Using XOR
+  # (0..nums.size).chain(nums).reduce(&:^)
+  
+  # Method 1: Sum formula
   n = nums.size
   n * (n + 1) / 2 - nums.sum
 end
