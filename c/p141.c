581107b03869277e21336517d7d7946458c9ffed
takavik <takavik@outlook.com>
2024-03-06 11:10:39 +0800

    Solve problem 141

---
+++ c/p141.c
@@ -0,0 +1,26 @@
+#include <stdbool.h>
+
+/**
+ * Definition for singly-linked list.
+ *
+ * */
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+bool hasCycle(struct ListNode *head) {
+    struct ListNode *slow, *fast;
+
+    slow = fast = head;
+    while (slow && fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast) {
+            return true;
+        }
+    }
+
+    return false;
+}
