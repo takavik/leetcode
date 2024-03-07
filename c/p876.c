31111e7d467e02fbcfc6cfb3ca7abdb4283195ef
takavik <takavik@outlook.com>
2024-03-07 13:37:57 +0800

    Solve problem 876

---
+++ c/p876.c
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* middleNode(struct ListNode* head) {
+    if (!head->next) {
+        return head;
+    }
+
+    struct ListNode *fast, *slow;
+
+    fast = slow = head;
+    while (fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    return slow;
+}
